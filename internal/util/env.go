a75a28e3f7c3a263c03bb33cee7ed91afed5d6d5
tjex <tjex@tjex.net>
2025-12-24 12:28:46 +0100

    allow DOTF_ENV variable to be unset, which falls back to "default" modules

--- internal/util/env.go
+++ internal/util/env.go
@@ -1,31 +1,22 @@
 package util
 
 import (
-	"errors"
-	"fmt"
-	"git.sr.ht/~tjex/dotf/internal/config"
 	"os"
+
+	"git.sr.ht/~tjex/dotf/internal/config"
 )
 
 var cfg = config.UserConfig()
 
 // Get the user set dotf environment. Prioritises shell env variable, DOTF_ENV.
 func ResolveEnvironment() string {
-
-	var env string
 	envConfig := cfg.Env
 	envShell := os.Getenv("DOTF_ENV")
-	if envConfig == "" && envShell == "" {
-		fmt.Println(errors.New("No dotf environment set."))
-		os.Exit(1)
-	}
-	if envConfig != "" && envShell != "" {
-		env = envShell
-	} else if envShell != "" {
-		env = envShell
-	} else {
-		env = envConfig
+	if envShell != "" {
+		return envShell
+	} else if envConfig != "" {
+		return envConfig
 	}
 
-	return env
+	return "default"
 }
